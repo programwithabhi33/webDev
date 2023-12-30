930967e05cec9fb0892be28a185df5301d7d0b36
abhi <programwithabhi@gmail.com>
2023-12-30 10:28:55 +0530

    classes basics in javascript

---
+++ classes/firstClass.js
@@ -0,0 +1,13 @@
+class Animal{
+    constructor(){
+        console.log("Object is creating");
+    }
+    eat(){
+        console.log("eat function");
+    }
+    jump(){
+        console.log("jump function");
+    }
+}
+let rabbit = new Animal();
+console.log(rabbit);
